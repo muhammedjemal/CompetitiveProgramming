bd4c7f7d299803da6ca4ebbe890fb7f7202d593f
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-01-21 23:58:44 +0300

    Create reduce-array-size-to-the-half.py

---
+++ reduce-array-size-to-the-half.py
@@ -0,0 +1,9 @@
+def minSetSize(self, arr):
+    count = Counter(arr)
+    currentCount = 0
+    currentSize = 0
+    for value in sorted(count.values(), key=None, reverse=True):
+        currentSize += 1
+        if currentCount + value >= len(arr) // 2:
+            return currentSize
+        currentCount += value
