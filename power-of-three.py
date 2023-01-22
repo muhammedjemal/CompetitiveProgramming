2cd5d58067c871b1e64bf6be170fe266d4230e54
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-01-23 00:09:16 +0300

    Update power-of-three.py

--- power-of-three.py
+++ power-of-three.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        x = 1
-        while x <= n:
-            if x != n:
-                x = x * 3
-            else:
-                return True
-        return False
+        if n <= 0:
+            return False
+        y = math.log(n , 3)
+        y = math.ceil(y)
+        return math.pow(3, y) == n
