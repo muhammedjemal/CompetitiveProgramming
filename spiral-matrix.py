0334262cd810220be2bc84ce109c341444d1d8a6
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-01-23 23:27:13 +0300

    Create spiral-matrix.py

---
+++ spiral-matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        while matrix:
+            result += matrix.pop(0)
+            matrix = list(zip(*matrix))[::-1]
+        return result
